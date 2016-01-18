35d38641eba6954677ebeefbaf7237d9309b8b83
Juan Matthys Uys <opyate@gmail.com>
2016-01-18 16:24:57 +0000

    add extra example to dot dash

--- .da.sh
+++ .da.sh
@@ -5,5 +5,9 @@
 # The parent script doesn't provide quotes, so you have to:
 #echo '"stringy"'
 
+# Extracting JSON across many *nix platforms?
+#APP_VERSION=$(cat package.json | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["version"]')
+#printf '{"version":"'$APP_VERSION'"}'
+
 # JSON is easy with printf
 printf '{"version": "1.1.3"}'
